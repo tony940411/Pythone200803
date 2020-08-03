b9c752aba6303fedc3f28aaaba3f0d4ee65729d5
tony940411 <69131764+tony940411@users.noreply.github.com>
2020-08-03 11:21:46 +0800

    Add files via upload

---
+++ Day 1-1.py
@@ -0,0 +1,9 @@
+print(" I'm Eric \nHe is 'cool'")
+input("tony")
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  3 10:06:14 2020
+
+@author: user
+"""
+
---
+++ Day 1-2.py
@@ -0,0 +1,9 @@
+name=input("enter in ")
+print(name +",Hi")
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  3 10:29:09 2020
+
+@author: user
+"""
+
