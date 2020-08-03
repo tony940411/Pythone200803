4c9e4701c505ff9b78eaa59728dd73c3fcd12f93
tony940411 <69131764+tony940411@users.noreply.github.com>
2020-08-03 11:56:03 +0800

    Add files via upload

---
+++ Day1-3.py
@@ -0,0 +1,14 @@
+r=input("請輸入圓的半徑")
+ans = float(r) *2*3.14
+print("圓的周長是", ans)
+r=input("請輸入圓的半徑")
+ans2=float(r)*float(r)*3.14
+print("圓的面積是", ans2)
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  3 10:35:07 2020
+
+@author: user
+"""
+
+
